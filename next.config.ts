45f921de3d3203cceeded8152da4ef698a49f06d
tetra-mix <ryouma1128@outlook.jp>
2024-11-13 21:09:04 +0900

    update: config content-collections

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import { withContentCollections } from "@content-collections/next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+nextConfig;
+
+export default withContentCollections(nextConfig);
