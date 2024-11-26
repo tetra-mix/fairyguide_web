a3385694c9572e1a9df1f07d00694a74a8fa39d1
tetra-mix <ryouma1128@outlook.jp>
2024-11-26 22:34:43 +0900

    update: contents & video section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import { ContentSection } from "../components/section/ContentsSection";
 import { SplideSection } from "../components/section/SplideSection";
 import { WhatSection } from "../components/section/WhatSection";
 import { RoleSection } from "../components/section/RoleSection";
-import { ReactElement } from "react";
+import { VideoSection } from "../components/section/VideoSection";
 
 export default function Home() {
   return (
     <>
       <SplideSection />
       <WhatSection />
+      <VideoSection />
       <RoleSection />
       <ContentSection />
     </>
---
+++ src/components/section/VideoSection.tsx
@@ -0,0 +1,22 @@
+import { SectionTitle } from "../atom/SectionTitle";
+import { FairyGuide } from "../atom/FairyGuide";
+import { Center } from "@yamada-ui/react";
+import { Container, Box, Text } from "@yamada-ui/react";
+
+export const VideoSection = () => {
+
+    return (
+        <Container p={{ base: 8, sm: 2 }} id="what">
+            <SectionTitle>
+                デモ動画
+            </SectionTitle>
+            <Center>
+                <Box mb={8} pb={4} w={{ base: "50vw", sm: "full" }}>
+                    <video controls>
+                        <source src="./demovideo_11_10.mp4" type="video/mp4" />
+                    </video>
+                </Box>
+            </Center>
+        </Container>
+    );
+}
